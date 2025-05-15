99775fec0ed09c9c096ef091c52c0d027eeebdf3
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-05-15 12:06:38 +0300

    creating order class

--- newproject/api/models.py
+++ newproject/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 class User(AbstractUser):
     pass
@@ -19,3 +20,19 @@ class Products(models.Model):
         return self.name
 
 
+class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING="pending"
+        CONFIRMED="confirmed"
+        CANCELLED="cnacelled"
+
+    order_id=models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    status=models.CharField(
+        max_length=10, choices=StatusChoices.choices,
+        default=StatusChoices.PENDING
+        )
+    def __str__(self):
+        return f"Order {self.order_id} by {self.user.username}"
+    
