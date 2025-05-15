b0e42a70692ae2fe4d2a5393320a759c9079d4c3
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-05-15 18:43:39 +0300

    Aggregating data

--- newproject/api/serializer.py
+++ newproject/api/serializer.py
@@ -18,3 +18,32 @@ class ProductSerializer(serializers.ModelSerializer):
                     'price must be greater than 0.'
                 )
             return value
+        
+class OrderItemSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model=OrderItem
+        fields=("product","quantity")
+        
+class OrderSerializer(serializers.ModelSerializer):
+    items=OrderItemSerialzer(many=True, read_only=True)
+    total_price=serializers.SerializerMethodField(method_name='total')
+
+    def total(self,obj):
+        order_items=obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
+    class Meta:
+        model=Order
+        fields=(
+            "order_id",
+            "created_at",
+            "user",
+            "status",
+            "items",
+            "total_price")
+        
+
+class productInfoSerializer(serializers.Serializer):
+    products=ProductSerializer(many=True)
+    count=serializers.IntegerField()
+    max_price=serializers.FloatField()
