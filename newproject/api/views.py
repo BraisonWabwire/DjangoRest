4dd94c3b287000c706e4c4106cd81cce933be68d
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-05-15 14:29:15 +0300

    creating id endpoint

--- newproject/api/views.py
+++ newproject/api/views.py
@@ -1,12 +1,18 @@
-from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from .serializer import ProductSerializer
 from .models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-
+@api_view(['GET'])
 def product_list(request):
     products=Product.objects.all()
     serializer=ProductSerializer(products, many=True)
-    return JsonResponse({
-        'data': serializer.data
-    })
+    return Response(serializer.data)
 
+
+@api_view(['GET'])
+def product_detail(request,pk):
+    product=get_object_or_404(Product, pk=pk)
+    serializer=ProductSerializer(product)
+    return Response(serializer.data)
