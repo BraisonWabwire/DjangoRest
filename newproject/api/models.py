c033afbfcdf37d853b3bb687cc87761bb7ed96fc
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-05-15 11:53:52 +0300

    product model

--- newproject/api/models.py
+++ newproject/api/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-class User(models.Model):
-    name=models.CharField(max_length=50)
-    age=models.IntegerField()
+class User(AbstractUser):
+    pass
 
+class Products(models.Model):
+    name=models.CharField(max_length=200)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    stock=models.PositiveBigIntegerField()
+    image=models.ImageField(upload_to='/products',blank=True,null=True)
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+    
     def __str__(self):
         return self.name
-    
+
+
