67af8b14e7544d88777c6e372cc7cb7eaa079725
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-05-15 12:28:01 +0300

    Order item model

--- newproject/api/models.py
+++ newproject/api/models.py
@@ -5,7 +5,7 @@ import uuid
 class User(AbstractUser):
     pass
 
-class Products(models.Model):
+class Product(models.Model):
     name=models.CharField(max_length=200)
     description=models.TextField()
     price=models.DecimalField(max_digits=10, decimal_places=2)
@@ -33,6 +33,19 @@ class Order(models.Model):
         max_length=10, choices=StatusChoices.choices,
         default=StatusChoices.PENDING
         )
+    products=models.ManyToManyField(Product, through="OrderItem", related_name='orders')
+
     def __str__(self):
         return f"Order {self.order_id} by {self.user.username}"
     
+class OrderItem(models.model):
+    order=models.ForeignKey(Order, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField()
+
+    @property
+    def item_subtotal(self):
+        return self.product.price * self.quantity
+    
+    def __str__(self):
+        return f"{self.quantity} X {self.product.name} in Order {self.order.order_id}"
